0176459cdd3f6155f6460d4e7616809f9b92b150
daniel buelvas <d.buelvasc@uniandes.edu.co>
2025-03-30 21:18:54 -0400

    docker file, requirements and config files

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+class Config:
+    SQLALCHEMY_DATABASE_URI = os.environ.get(
+        "DATABASE_URL", "postgresql://myuser:mypass@localhost:5432/mydatabase"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SAFRS_API_DOCS = True
+    SAFRS_API_DOCS_URL = os.environ.get("SAFRS_API_DOCS_URL", "/api/swagger.html")
+    HOST = os.environ.get("HOST", "localhost")
+    PORT = int(os.environ.get("PORT", 8084))
