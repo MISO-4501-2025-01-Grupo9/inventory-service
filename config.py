7893745a62d4e4365adbdf7de6b1c60e018934a7
Felix Ernesto Orduz Grimaldo <f.orduz@uniandes.edu.co>
2025-04-03 21:45:52 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SAFRS_API_DOCS = True
     SAFRS_API_DOCS_URL = os.environ.get("SAFRS_API_DOCS_URL", "/api/swagger.html")
-    HOST = os.environ.get("HOST", "localhost")
-    PORT = int(os.environ.get("PORT", 8084))
+    HOST = os.environ.get("HOST", "0.0.0.0")
+    PORT = int(os.environ.get("PORT", 8080))
